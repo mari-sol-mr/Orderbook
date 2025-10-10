53745b630f2b33bc9beb84d858cadf3aca3779c6
mari-sol-mr <119021621+mari-sol-mr@users.noreply.github.com>
2025-10-10 01:40:31 +0000

    outline

---
+++ OrderBookB.h
@@ -0,0 +1,141 @@
+
+#include "OrderBookTypes.h"
+#include <vector>
+#include <map>
+
+// Generic max heap and min heap templates
+#include <queue>
+#include <functional>
+#include <thread>
+
+
+
+class OrderBookB
+{
+public:
+    OrderBookB()
+    : totalOrders_ { 0 }
+    { 
+        orderHistory_.reserve(100000000);
+    }
+
+    void submitOrderRequest(OrderType orderType, MarketSide marketSide, Price price, Quantity quantity)
+    {
+        OrderPointer newOrder = createOrder(orderType, marketSide, price, quantity);
+
+        logOrder(newOrder);
+
+        matchOrder(newOrder);
+
+    }
+
+    OrderPointer createOrder(OrderType orderType, MarketSide marketSide, Price price, Quantity quantity)
+    {
+        OrderId orderId = generateOrderId();
+
+        return std::make_shared<Order>(orderId, orderType, marketSide, price, quantity);
+    }
+
+    OrderId generateOrderId()
+    {
+        return ++totalOrders_;
+    }
+
+    void logOrder(OrderPointer order)
+    {
+        orderHistory_.push_back(order);
+    }
+
+    void logTrade(TradePointer trade)
+    {
+        tradeHistory_.push_back(trade);
+    }
+
+        // todo: mayeb there shouldn't be a difference bw orders? just order1 and order2?
+    TradePointer executeTrade(OrderPointer order1, OrderPointer order2)
+    {
+        Quantity tradeQuantity = std::min(order1->getRemainingQuantity(), order2->getRemainingQuantity());
+        std::cout << "tradequantity: " <<tradeQuantity << "\n";
+        order1->fillOrder(tradeQuantity);
+        order2->fillOrder(tradeQuantity);
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        return std::make_shared<Trade>(order1->getPrice(), order2->getOrderId(), order1->getOrderId(), tradeQuantity);
+    }
+
+    void matchOrder(OrderPointer order)
+    {
+        std::string ordertype = order->getMarketSide() == MarketSide::Buy ? " buy order " : " sell order ";
+        std::cout << "\n\nmatching " << ordertype << order->getOrderId() << "\n";
+        OrdersQueue* unfilledOrders = order->getMarketSide() == MarketSide::Buy ? &unfilledAsks_[order->getPrice()] : &unfilledBids_[order->getPrice()]; // todo: refactor. potentialMatches?
+
+        bool matchAvailable = !unfilledOrders->empty();
+        if (matchAvailable) std::cout << "there is an unfilled order \n";
+        while(!order->isFilled() && matchAvailable)
+        {
+            OrderPointer bestMatch = unfilledOrders->front();
+            std::cout << "best match: " << bestMatch->getOrderId() << "\n";
+
+            TradePointer trade = executeTrade(order, bestMatch);
+            
+            logTrade(trade); // todo: maybe there is some way to optimize this and reserve x space?
+
+            if (bestMatch->isFilled())
+            {
+                std::cout<< "best match filled\n";
+                unfilledOrders->pop();
+            }
+
+            matchAvailable = !unfilledOrders->empty();
+
+            if (!matchAvailable) std::cout << "all opposite side orders filled\n";
+        }
+
+        if (!order->isFilled())
+        {
+            std::cout<< "queueing order, it is not filled yet\n"; 
+            queueOrder(order);
+        }
+
+            matchAvailable = !unfilledOrders->empty();
+
+            if (!matchAvailable) std::cout << "check 2: all opposite side orders filled\n";
+
+            OrdersQueue bids_check =  unfilledBids_[order->getPrice()];
+            std::cout<< "checking remaining bids: ";
+            if (bids_check.empty() ) std::cout << "bids are empty\n";
+            else std::cout << "bids are not empty\n";
+    }
+
+    void queueOrder(OrderPointer order)
+    {
+        if (order->getMarketSide() == MarketSide::Buy) 
+            unfilledBids_[order->getPrice()].push(order);
+        else  
+            unfilledAsks_[order->getPrice()].push(order);
+    }
+
+    void printTradeHistory()
+    {
+        std::cout << "printing hist\n";
+        for (const auto& trade : tradeHistory_)
+        {
+            std::cout << "Traded quantity " << trade->getQuantity() << " at price " << trade->getPrice()
+            << " with orders " << trade->getAskOrderId() << " and " << trade->getBidOrderId() << "\n"; // todo: create getters
+        }
+    }
+
+
+private:
+    int totalOrders_;
+    OrderPointers orderHistory_; // todo: refactor to orders list
+    TradePointers tradeHistory_; // todo: refactor to trades list
+    std::map<Price, OrdersQueue, std::less<Price>> unfilledAsks_;
+    std::map<Price, OrdersQueue, std::greater<Price>> unfilledBids_;
+};
+
+
+    
+
+// g++ OrderBookB.cpp -o OrderBookB
---
+++ OrderBookTest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include "OrderBook.h"
+#include "OrderBookB.h"
+
+using namespace std;
+
+int main()
+{
+    OrderBookB ob;
+
+    
+
+    ob.submitOrderRequest(OrderType::Limit, MarketSide::Sell, 5, 1);
+    ob.submitOrderRequest(OrderType::Limit, MarketSide::Buy, 5, 1);
+    ob.submitOrderRequest(OrderType::Limit, MarketSide::Sell, 5, 1);
+    ob.submitOrderRequest(OrderType::Limit, MarketSide::Buy, 5, 1);
+
+    
+    ob.printTradeHistory();
+    return 0;
+}
+//g++ OrderBookTest.cpp -o OrderBookTest
